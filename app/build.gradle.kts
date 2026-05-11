6f36f1d6e177a1748fef2b247a891c2cf88ee558
DanyloM73 <danyamalanin7@gmail.com>
2026-05-11 21:24:55 +0300

    Updated version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.danylom73.rescuehelper"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
